640082905c3f70dc26f6092d38893b194f5a43cb
Dhruv Maheshwari <dhwari@gmail.com>
2013-12-18 03:03:15 -0500

    Revert 63626f3..91c059c
    This rolls back to commit 63626f331f0705aa1c0cf2b14cd0a4b40566250a.

--- routes/routes.js
+++ routes/routes.js
@@ -51,19 +51,21 @@ var createAccount = function(req, res) {
 	var last_name = req.body.last_name;
 	var school = req.body.school;
 	var birthday = req.body.birthday;
+	var gender = req.body.gender;
+	var interests = req.body.interests;
 	if (email === "" || password1 === "" || password2 === ""
 			|| first_name === "" || last_name === "" || school === "") {
 		res.render("signup.ejs", {error : 1});
 	} else if (password1 != password2) {
 		res.render("signup.ejs", {error : 2});
 	} else {
-		db.createAccount(email, password1, first_name, last_name, school, birthday, function(err, data) {
+		db.createAccount(email, password1, first_name, last_name, school, birthday, gender, interests, function(err, data) {
 			if (err) {
 				res.render("signup.ejs", {error : err.value});
 			} else {
 				req.session.logged = true;
 				console.log(data);
-				req.session.username = data.Attributes[0].Value;
+				req.session.username = data;
 				res.redirect("/profile/76705186394974");
 			}
 		});
@@ -92,6 +94,10 @@ var getProfile = function(req, res){
 						var firstName = data[j].Value;
 					} else if(data[j].Name=="last_name") {
 						var lastName = data[j].Value;
+					} else if(data[j].Name=="gender") {
+						var gender = data[j].Value;
+					} else if(data[j].Name=="interests") {
+						var interests = data[j].Value;
 					}
 				}
 				profile.firstName = firstName;
@@ -100,6 +106,8 @@ var getProfile = function(req, res){
 				profile.network = network;
 				profile.email = email;
 				profile.id = id;
+				profile.gender = gender;
+				profile.interests = interests;
 				
 				res.render("profile.ejs", {profile : profile});
 		}
@@ -226,7 +234,6 @@ function loadPostsWithUserIds(friend_ids, callback) {
             if (err) return callback(err);
             callback(null, posts);
         });
-	res.send("stuff");
 };
 
 //fetches wall posts for a profile page
@@ -237,12 +244,9 @@ var loadWall = function(req, res) {
 	db.loadWall(id, function(err, data){
 		if(err){
 			res.send("error on restaurants");
-<<<<<<< HEAD
-		} else if(data != undefined) {
-=======
-		} else if(data) {
->>>>>>> parent of ebcd4c2... search suggestions & signup
+		} else if(data.Items != undefined) {
 			posts = [];
+			if(data.Items != undefined){
 			for(i=0; i<data.Items.length; i++){
 				nextPost = new Object();
 				var name = data.Items[i].Name;
@@ -273,6 +277,7 @@ var loadWall = function(req, res) {
 			console.log(JSON.stringify(posts));
 			res.send(JSON.stringify(posts));			
 		}
+	}
 	});
 };
 
@@ -325,12 +330,10 @@ var checkFriend = function(req, res) {
 //fetches comments
 var loadComment = function(req, res) {
 	var id = req.params.id;
-	console.log("we reached routes");
 	db.loadComment(id, function(err, data){
 		if(err){
 			res.send("error on restaurants");
 		} else if(data) {
-			console.log("routes gets data from db");
 			comments = [];
 			if(data.Items !== undefined) {
 				for(i=0; i<data.Items.length; i++){
@@ -354,16 +357,33 @@ var loadComment = function(req, res) {
 					nextComment.like_count = like_count;
 					nextComment.post_tag = post_tag;
 					nextComment.creator = creator;
-					comments.push(nextComment);	
-				}
+					comments.push(nextComment);
+					
+					}
 			}
 			res.send(JSON.stringify(comments));			
-		} else {
-			res.send(JSON.stringify("{something}"));
 		}
 	});
 }; 
 
+var searchSuggest = function(req, res) {
+	var term = req.params.term;
+	db.findUsers(term, function(err, data) {
+		if (err) {
+			res.send("error during add");
+		} else if (data.Items != undefined) {
+			var arr = new Array();
+			console.log(data.Items[0].Attributes.length);
+			for (var i = 0; i < data.Items.length; i++) {
+				console.log(data.Items[i].Attributes[0].Value);
+				arr[i] = data.Items[i].Name + ',' + data.Items[i].Attributes[0].Value + ',' + data.Items[i].Attributes[1].Value;
+			}
+			res.send('search.ejs', {error:null, elements:arr});
+		} else {
+			res.send('search.ejs', {error:null, elements:null});
+		}
+	})
+}
 
 
 //after logout button is pressed
@@ -371,6 +391,10 @@ var logout = function(req, res) {
 	req.session.logged = false;
 	res.redirect('/');
 };
+
+var search = function(req, res) {
+	res.render('search.ejs', {error:null});
+}
 
 
 var routes = { 
@@ -387,7 +411,9 @@ var routes = {
   load_comments:loadComment,
   add_friend: addFriend,
   check_friend: checkFriend,
-  delete_friend: deleteFriend
+  delete_friend: deleteFriend,
+  search_help: searchSuggest,
+  get_search: search
 };
 
 module.exports = routes;
